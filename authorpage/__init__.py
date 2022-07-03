f04216663cdae4d398e9d470e4e873d3647e66e0
Ammon Smith <ammon.i.smith@gmail.com>
2022-07-03 09:00:37 -0400

    Update copyright years.

--- authorpage/__init__.py
+++ authorpage/__init__.py
@@ -2,7 +2,7 @@
 # __init__.py
 #
 # scp-author-page - Tools for generating my author page.
-# Copyright (c) 2021 Ammon Smith
+# Copyright (c) 2021-2022 Ammon Smith
 #
 # scp-author-page is available free of charge under the terms of the MIT
 # License. You are free to redistribute and/or modify it under those
